c4b7ea9099b41f1ce3a1b5b3b7f0a518dff8bdeb
khuram2025 <khuram2025@gmail.com>
2022-09-17 19:32:45 +0300

    profle added

--- channab/accounts/models.py
+++ channab/accounts/models.py
@@ -83,3 +83,23 @@ class User(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, blank = True, null = True)
+    profile_picture = models.ImageField(upload_to = 'users/profiles_pictures', blank = True, null = True)
+    cover_photo = models.ImageField(upload_to = 'users/profiles_pictures', blank = True, null = True)
+    address_line_1 = models.CharField(max_length=50, blank = True, null = True)
+    address_line_2 = models.CharField(max_length=50, blank = True, null = True)
+    country = models.CharField(max_length=50, default = "Pakistan")
+    state = models.CharField(max_length=50, blank = True, null = True)
+    city = models.CharField(max_length=50, blank = True, null = True)
+    pin_code = models.CharField(max_length=50, blank = True, null = True)
+    latitude = models.CharField(max_length=50, blank = True, null = True)
+    longitude = models.CharField(max_length=50, blank = True, null = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
