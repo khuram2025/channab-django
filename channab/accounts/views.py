3332f19697e26ca7ea92331f50166b1525ac871e
khuram2025 <khuram2025@gmail.com>
2022-09-24 18:56:11 +0300

    ERP Project

--- channab/accounts/views.py
+++ channab/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from multiprocessing import context
+import re
+from django.shortcuts import render, redirect
+from django.shortcuts import HttpResponse
+
+from accounts.forms import UserForm
 
 # Create your views here.
+
+
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("store")
+
+    else:
+        form = UserForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'account/registeruser.html', context)
---
+++ erp/authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from .views import LoginView, LogoutView, RegistrationView, UsernameValidationView
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+    path('register', RegistrationView.as_view(), name = 'register'),
+    path('login', LoginView.as_view(), name = 'login'),
+    path('logout', LogoutView.as_view(), name = 'logout'),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name = 'validate-username'),
+    
+]
